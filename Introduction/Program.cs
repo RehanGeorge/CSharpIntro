dfedb58981bb8b11668a47d14cc5917015203783
RehanGeorge <rehan.george@gmail.com>
2023-11-28 13:24:06 +0530

    Added IfElse method

--- Introduction/Program.cs
+++ Introduction/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IfElse();
+        }
+        public static void IfElse()
+        {
+            Console.WriteLine("What is the temperature like? Enter a number.");
+            int temperature;
+            try
+            {
+                temperature = int.Parse(Console.ReadLine());
+                if (temperature < 20)
+                {
+                    Console.WriteLine("Take the coat");
+                }
+                if (temperature == 20)
+                {
+                    Console.WriteLine("It's 20 degrees celsius. You should be fine");
+                }
+                else if (temperature > 20)
+                {
+                    Console.WriteLine("Wear something light");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format Exception, please enter a number");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong");
+            }
         }
     }
 }
